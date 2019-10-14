44380effca13da60cd689cc3355b8258d57f8562
hyojin <jin33032@gmail.com>
2019-10-14 23:56:32 +0900

    Create DiffUtil

---
+++ app/src/main/java/com/repo/gitawards/util/DiffUtil.kt
@@ -0,0 +1,18 @@
+package com.repo.gitawards.util
+
+import androidx.recyclerview.widget.DiffUtil
+
+class DiffUtil(
+    private val oldList : List<Any>,
+    private val newList : List<Any>
+) : DiffUtil.Callback() {
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? = oldList[oldItemPosition].equals(newList[newItemPosition])
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition].equals(newList[newItemPosition])
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition] == newList[newItemPosition]
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+}
