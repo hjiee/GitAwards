1f306df9f86c7a15dba5ad22f41b7d3a5ca8d76a
hyojin <jin33032@gmail.com>
2019-11-07 22:48:31 +0900

    Refactor RepositoryView

--- app/src/main/java/com/github/gitawards/ui/github/repositories/RepositoriesFragment.kt
+++ app/src/main/java/com/github/gitawards/ui/github/repositories/RepositoriesFragment.kt
@@ -1,11 +1,22 @@
-package com.github.gitawards.ui.github
+package com.github.gitawards.ui.github.repositories
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
 import com.github.gitawards.R
 import com.github.gitawards.base.BaseFragment
 import com.github.gitawards.databinding.FragmentSearchLanguageBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RepositoriesFragment : BaseFragment<FragmentSearchLanguageBinding>(R.layout.fragment_search_language) {
+
+    val viewModel by viewModel<RepositoryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     companion object {
         fun newInstance() = RepositoriesFragment().apply {
