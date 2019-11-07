2bfde017797b1ba81c5da018f31789cb012ad00f
hyojin <jin33032@gmail.com>
2019-11-07 22:47:17 +0900

    Create Login View

---
+++ app/src/main/java/com/github/gitawards/ui/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.github.gitawards.ui
+
+import com.github.gitawards.R
+import com.github.gitawards.base.BaseActivity
+import com.github.gitawards.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+}
