4fe2d671195456aad001e9f0d41e74093dff16ae
hyojin <jin33032@gmail.com>
2019-11-07 22:46:39 +0900

    Create RepositoryViewModel

---
+++ app/src/main/java/com/github/gitawards/ui/github/repositories/RepositoryViewModel.kt
@@ -0,0 +1,11 @@
+package com.github.gitawards.ui.github.repositories
+
+import com.github.gitawards.base.BaseViewModel
+import com.github.gitawards.data.GithubRepository
+
+class RepositoryViewModel(private val repository: GithubRepository) : BaseViewModel() {
+
+    fun load() {
+
+    }
+}
