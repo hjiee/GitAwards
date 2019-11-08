389232f3fa013a124a94ec423d38827a31695369
hyojin <jin33032@gmail.com>
2019-11-08 22:46:22 +0900

    Refactor Di ViewModelModule

--- app/src/main/java/com/github/gitawards/di/ViewModelModule.kt
+++ app/src/main/java/com/github/gitawards/di/ViewModelModule.kt
@@ -1,9 +1,15 @@
 package com.github.gitawards.di
 
 import com.github.gitawards.ui.main.MainViewModel
+import com.github.gitawards.ui.github.language.LanguageViewModel
+import com.github.gitawards.ui.github.repositories.RepositoryViewModel
+import com.github.gitawards.ui.github.users.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MainViewModel(get()) }
+    viewModel { LanguageViewModel(get()) }
+    viewModel { RepositoryViewModel(get()) }
+    viewModel { UserViewModel(get()) }
 }
