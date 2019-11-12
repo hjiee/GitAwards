e47149f63bb599e4790af5afef10189c59b5505a
hyojin <jin33032@gmail.com>
2019-11-12 22:22:38 +0900

    Refactor User VIewModel

--- app/src/main/java/com/github/gitawards/ui/github/users/UserViewModel.kt
+++ app/src/main/java/com/github/gitawards/ui/github/users/UserViewModel.kt
@@ -1,35 +1,51 @@
 package com.github.gitawards.ui.github.users
 
-import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.github.gitawards.base.BaseViewModel
 import com.github.gitawards.data.GithubRepository
-import com.github.gitawards.network.model.GithubResponse
+import com.github.gitawards.network.model.UserResponse
+import com.github.gitawards.network.model.UserSearchResponse
 
 class UserViewModel(private val repository: GithubRepository) : BaseViewModel() {
 
-    private val _githubInfo = MutableLiveData<List<GithubResponse.Items>>()
-    val githubInfo: LiveData<List<GithubResponse.Items>> get() = _githubInfo
+    private val _userInfo = MutableLiveData<List<UserResponse.Items>>()
+    val userInfo: LiveData<List<UserResponse.Items>> get() = _userInfo
+
+    private val _userInfo2 = MutableLiveData<List<UserSearchResponse>>()
+    val userInfo2: LiveData<List<UserSearchResponse>> get() = _userInfo2
 
     // refresh면 true / 아니면 false
     private val _rvRefresh = MutableLiveData<Boolean>()
-    val rvRefresh : LiveData<Boolean> get() = _rvRefresh.apply { false }
+    val rvRefresh: LiveData<Boolean> get() = _rvRefresh.apply { false }
 
     // 결과값이 null이면 true, null이 아니면 false
     private val _stateIsEmpty = MutableLiveData<Boolean>()
     val stateIsEmpty: LiveData<Boolean> get() = _stateIsEmpty.apply { true }
 
-    fun load(page : Int = 0) {
+    fun load(page: Int = 0) {
         repository.loadUser(
             type = "hjiee",
             page = page,
-            success = {
-
+            success = { data ->
+                _userInfo.value = data
             },
             failure = {
-
+                _userInfo.value = emptyList()
             })
 
+    }
+
+    fun searchUser(page: Int = 0) {
+        repository.searchUser(
+            since = "0",
+            page = page,
+            success = { data ->
+                _userInfo2.value = data
+            },
+            failure = {
+                _userInfo.value = emptyList()
+            }
+        )
     }
 }
--- app/src/main/java/com/github/gitawards/ui/github/users/UsersFragment.kt
+++ app/src/main/java/com/github/gitawards/ui/github/users/UsersFragment.kt
@@ -2,26 +2,53 @@ package com.github.gitawards.ui.github.users
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.view.ViewGroup
+import androidx.databinding.library.baseAdapters.BR
+import com.github.gitawards.BaseRecyclerView
 import com.github.gitawards.R
 import com.github.gitawards.base.BaseFragment
-import com.github.gitawards.databinding.FragmentSearchLanguageBinding
+import com.github.gitawards.databinding.FragmentUserBinding
+import com.github.gitawards.network.model.UserResponse
+import com.github.gitawards.network.model.UserSearchResponse
+import kotlinx.android.synthetic.main.fragment_user.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UsersFragment :
-    BaseFragment<FragmentSearchLanguageBinding>(R.layout.fragment_search_user) {
+    BaseFragment<FragmentUserBinding>(R.layout.fragment_user) {
 
-    val viewModel by viewModel<UserViewModel>()
-
-
+    private val viewModel by viewModel<UserViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.load()
+//        viewModel.load()
+        viewModel.searchUser()
 
+    }
+
+    override fun initEventHandler() {
+
+    }
+
+    override fun initBinding() {
+        binding.run {
+            vm = viewModel
+            rvGithubUser.apply {
+                adapter = object : BaseRecyclerView.Adapter<UserSearchResponse,FragmentUserBinding>(
+                    R.layout.recycler_user,
+                    BR.response
+                ) {
+                    override fun onCreateViewHolder(
+                        parent: ViewGroup,
+                        viewType: Int
+                    ): BaseRecyclerView.ViewHolder<FragmentUserBinding> {
+                        return super.onCreateViewHolder(parent, viewType).apply {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 
     companion object {
