516d5cda9b9fbf1025607b4c466ff605efaad3ad
hyojin <jin33032@gmail.com>
2019-11-13 20:42:17 +0900

    Refactor Repository

--- app/src/main/java/com/github/gitawards/data/GithubRepository.kt
+++ app/src/main/java/com/github/gitawards/data/GithubRepository.kt
@@ -2,7 +2,9 @@ package com.github.gitawards.data
 
 import com.github.gitawards.data.source.GithubDataSource
 import com.github.gitawards.network.api.GithubApi
-import com.github.gitawards.network.model.GithubLanguageResponse.Items
+import com.github.gitawards.network.model.GithubResponse
+import com.github.gitawards.network.model.UserResponse
+import com.github.gitawards.network.model.UserSearchResponse
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -12,7 +14,7 @@ class GithubRepository(val githubApi: GithubApi) : GithubDataSource {
     override fun loadLanguage(
         type: String?,
         page: Int,
-        success: (List<Items>?) -> Unit,
+        success: (List<GithubResponse.Items>?) -> Unit,
         failure: (String) -> Unit
     ): Disposable {
         return githubApi.getLanguage("language:${type}", "stars", "desc", page)
@@ -30,14 +32,31 @@ class GithubRepository(val githubApi: GithubApi) : GithubDataSource {
     override fun loadUser(
         type: String?,
         page: Int,
-        success: (List<Items>?) -> Unit,
+        success: (List<UserResponse.Items>?) -> Unit,
         failure: (String) -> Unit
     ): Disposable {
-        return githubApi.getUsers("q:${type}","repositories","desc",page)
+        return githubApi.getUsers("$type", "", "desc", page)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 success(it.items)
+            }, {
+                failure(it.toString())
+            })
+    }
+
+
+    override fun searchUser(
+        since: String,
+        page: Int,
+        success: (List<UserSearchResponse>?) -> Unit,
+        failure: (String) -> Unit
+    ): Disposable {
+        return githubApi.searchUser(since,page)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                success(it)
             }, {
                 failure(it.toString())
             })
